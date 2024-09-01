c297e8464931175ce0cfc46601b653a31f2172e9
Apex <seetaramaraju.jampana@gmail.com>
2024-09-01 05:17:01 -0500

    leetcode problem 1046

---
+++ last_stone_Weight_1046.py
@@ -0,0 +1,9 @@
+class Solution:
+    def lastStoneWeight(self, stones: List[int]) -> int:
+        stones.sort()
+        while len(stones) > 1:
+            stone_1= stones.pop()
+            stone_2= stones.pop()
+            if stone_1 != stone_2 :
+                bisect.insort(stones, abs(stone_2 - stone_1))
+        return stones[0] if stones else 0 
